128455dcb0fd8a60762d95626df49cb4cf8d33ec
Bryan Lee <bryanleeyensheng@gmail.com>
2024-08-02 11:19:36 -0500

    spacing between name and title

--- src/HeroPage.tsx
+++ src/HeroPage.tsx
@@ -21,7 +21,7 @@ const HeroPage = () => {
 
       {/* Title and Role */}
       <div className="title_container">
-        <h1>Hi,I'm Bryan.</h1>
+        <h1>Hi,I'm Bryan.</h1><br/>
         <h2>
           <TypeAnimation
             sequence={[
