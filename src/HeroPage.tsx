5a038242c1c38ac83935cb859006e1807173b4fd
Bryan Lee <63344458+bryanlee882001@users.noreply.github.com>
2024-08-04 17:13:37 -0500

    Update HeroPage.tsx

--- src/HeroPage.tsx
+++ src/HeroPage.tsx
@@ -52,7 +52,7 @@ const HeroPage = () => {
       <div className="description_container">
         <p>Based in Minneapolis, MN. I'm currently a Data Engineer Intern @ <a href="https://www.bfcsoftware.com/" target="blank">BFC Software </a>
         where I build and optimize data pipelines to scale with our growing infrastructure. I am particularly interested in distributed
-        computing and big data processing.</p>
+        computing and big data processing/analytics.</p>
 
         <br/><br/>
 
@@ -60,13 +60,13 @@ const HeroPage = () => {
         of the <a href="https://github.com/bryanlee882001/AIMSES" target="blank">Auroral Ionospheric Magnetospheric Statistical Electron Spectra (AIMSES)</a> project for real-time statistical computation of collected 
         spectral data from <a href="https://nssdc.gsfc.nasa.gov/nmc/spacecraft/display.action?id=1996-049A" target="blank">NASA's FAST satellite</a> @ 
         the <a href="https://cse.umn.edu/physics" target="blank">School of Physics & Astronomy</a>, 
-        as well as assisting in the development of ECG compression algorithms and data tools for cardiac monitoring @ <a href="https://dcsg.cs.umn.edu/" target="blank">Distributed Computing Systems Group</a>.</p>
+        as well as assisting in the development of Electrocardiogram (ECG) compression algorithms and data tools for cardiac monitoring @ <a href="https://dcsg.cs.umn.edu/" target="blank">Distributed Computing Systems Group</a>.</p>
 
         <br/><br/>
 
         <p>I've previously worked as a Solution Analyst @ <a href="https://www.genustechnologies.com/" target="blank">Genus Technologies</a> and
          a Software Development Engineer Intern @ <a href="https://www.keysight.com/us/en/home.html" target="blank">Keysight Technologies</a> where
-         I collaborated with interdiscplinary teams to develop and enhance software solutions which drives impactful results. 
+         I collaborated with interdisciplinary teams to develop and enhance software solutions that drive impactful results. 
         </p>
 
         <br/><br/>
